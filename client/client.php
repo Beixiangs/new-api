2c1ce5e936ae30f348feaa356749966f83c29c8e
hjx <17660302664@163.com>
2018-09-19 15:31:42 +0800

    数据标准输出完毕

--- client/client.php
+++ client/client.php
@@ -1,5 +1,20 @@
 <?php
+	
+	include './vendor/autoload.php';
+
+	// 请求的api地址
+	$url = 'http://localhost//api/api/server/server.php';
+
+	$curl = new Curl\Curl();
+	$curl->get($url);
+
+	if($curl->error){
+		echo $curl->error_code;
+	}else{
+		echo $curl->response;
+	}
 
 	// 客户端 发起api请求
+	
 
 ?>
--- server/server.php
+++ server/server.php
@@ -1,5 +1,23 @@
 <?php
 
 	// 接受请求 相应数据 json
+	$pdo = new PDO('mysql:host=localhost;dbname=api;charset=utf8','root','');
+	
+	$stmt = $pdo->query('select *from user');
+	$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	// 定义标准化产出数据格式函数
+	function resp($data,$status,$message){
+
+		$res = [
+			'status'=>$status,// 服务器响应状态码 
+			'message'=>$message,// 此次api请求的描述
+			'data'=>$data
+		];
+
+		return json_encode($res,JSON_UNESCAPED_UNICODE);
+	}
+
+	echo resp($data,200,'ok');
 
 ?>
